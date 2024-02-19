557c0534736d3e3d1434785c49da26037a816d9a
iamgufran <mgfzd786@gmail.com>
2024-02-20 00:27:18 +0530

    Add register route

--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt'
+const bcrypt = require('bcrypt')
 
-export const hashPassword = async(password) => {
+const hashPassword = async(password) => {
     try{
         //determines the complexity and computational cost of the password hashing process
         const saltRounds =10;
@@ -12,6 +12,11 @@ export const hashPassword = async(password) => {
     }
 };
 
-export const comparePassword = async(password, hashedPassword) => {
+const comparePassword = async(password, hashedPassword) => {
     return bcrypt.compare(password, hashedPassword);
 };
+
+module.exports = {
+    hashPassword,
+    comparePassword
+}
