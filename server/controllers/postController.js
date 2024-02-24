cacb410e2bc4a2b1f9f1e98f7224d39b98fdf74c
iamgufran <mgfzd786@gmail.com>
2024-02-24 23:34:49 +0530

    Post routes to be checked by faiz

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,5 +1,6 @@
-const userModel = require("../models/postModel.js");
+const postModel = require("../models/postModel.js");
 
+//add post
 const postController = async (req, res) => {
   try {
     const { title, description, image_url, likes, company_name, package, role, status, overall_exp } = req.body;
@@ -21,7 +22,7 @@ const postController = async (req, res) => {
     }
 
     //save
-    const user = await new userModel({
+    const post = await new postModel({
       title,
       description,
       image_url,
@@ -36,7 +37,7 @@ const postController = async (req, res) => {
     res.status(201).send({
       success: true,
       message: "posted",
-      user,
+      post,
     });
   } catch (error) {
     console.log(error);
@@ -48,4 +49,40 @@ const postController = async (req, res) => {
   }
 };
 
-module.exports = {postController}
+//edit post
+const editController = async (req, res) => {
+  try {
+    const { title, description, image_url, likes, company_name, package, role, status, overall_exp } = req.body;
+    const post = await postModel.findById(req.post._id);
+    
+    const updatedPost = await postModel.findByIdAndUpdate(
+      req.post._id,
+      {
+        title: title || post.title,
+        description: description || post.description,
+        image_url: image_url || post.image_url,
+        likes: 0,
+        company_name: company_name || post.company_name,
+        package: package || post.package,
+        role: role || post.role,
+        status: status || post.status,
+        overall_exp: overall_exp || post.overall_exp
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Post edited SUccessfully",
+      updatedPost,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error WHile Update profile",
+      error,
+    });
+  }
+};
+
+module.exports = {postController, editController}
--- server/routes/addRoutes.js
+++ server/routes/addRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { postController } = require('../controllers/postController.js');
+const { editController, postController } = require('../controllers/postController.js');
 
 // Create router object
 const router = express.Router();
 
 // Define routes
 router.post('/add-post', postController);
+router.post('/edit-post', editController);
 
 module.exports = router;
